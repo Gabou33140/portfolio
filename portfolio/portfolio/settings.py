813263abdac6867bccb51d56e3da9c4507e851df
godillar <gabriel.odillard@outlook.fr>
2023-08-03 05:43:52 +0200

    maj settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,12 +112,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Media files
+MEDIA_URL = '/medias/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),
+    # Ajoutez d'autres répertoires static pour chaque application ici
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
